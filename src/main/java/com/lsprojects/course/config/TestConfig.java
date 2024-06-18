d8b4e5883398c186877c65fb45bb21c5a427bb80
Railan Santana <lansilvah14fsa@gmail.com>
2024-06-18 16:03:08 -0300

    feat: creat enum OrderStatus and implementation in Order

--- src/main/java/com/lsprojects/course/config/TestConfig.java
+++ src/main/java/com/lsprojects/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.lsprojects.course.entities.Order;
 import com.lsprojects.course.entities.User;
+import com.lsprojects.course.entities.enums.OrderStatus;
 import com.lsprojects.course.repositories.OrderRepository;
 import com.lsprojects.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Little Doe", "littleDoe@example.com", "988887777", "sisos");
 		User u3 = new User(null, "Big Joe", "bj@example.com", "999990101", "migos");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u3);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u3);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAIMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
